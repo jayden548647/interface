a30b9593b41a1df0269355e6ddb4652a67f0b03f
jayden548647 <548647@chrome.escg.ac.uk>
2025-01-14 11:14:35 +0000

    audio settings are finally fixed

--- Assets/scripts/MenuScript.cs
+++ Assets/scripts/MenuScript.cs
@@ -3,28 +3,67 @@ using UnityEngine.Audio;
 
 public class MenuScript : MonoBehaviour
 {
+    GameObject Strat;
     float masterVol;
     float musicVol;
     float SFXVol;
+    float noSet;
     public AudioMixer mixer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        //get playerprefs settings
+        Strat = GameObject.FindGameObjectWithTag("Menu");
+        
 
         masterVol = PlayerPrefs.GetFloat("MasterVolume");
         musicVol = PlayerPrefs.GetFloat("MusicVolume");
         SFXVol = PlayerPrefs.GetFloat("SFXVolume");
 
-        mixer.SetFloat("MasterVol", Mathf.Log10(masterVol));
-        mixer.SetFloat("MusicVol", Mathf.Log10(musicVol));
-        mixer.SetFloat("SFXVol", Mathf.Log10(SFXVol));
-
+        
+       
+    }
+    private void Start()
+    {
+        noSet = 4;
+        if (PlayerPrefs.GetFloat("MasterVolume") == 0)
+        {
+            mixer.SetFloat("MasterVol", -80);
+        }
+        if (PlayerPrefs.GetFloat("MusicVolume") == 0)
+        {
+            mixer.SetFloat("MusicVol", -80);
+        }
+        if (PlayerPrefs.GetFloat("SFXVolume") == 0)
+        {
+            mixer.SetFloat("SFXVol", -80);
+        }
+        if (PlayerPrefs.GetFloat("MasterVolume") > 0)
+        {
+            mixer.SetFloat("MasterVol", Mathf.Log10(masterVol));
+        }
+        if (PlayerPrefs.GetFloat("MusicVolume") > 0)
+        {
+            mixer.SetFloat("MusicVol", Mathf.Log10(musicVol));
+        }
+        if (PlayerPrefs.GetFloat("SFXVolume") > 0)
+        {
+            mixer.SetFloat("SFXVol", Mathf.Log10(SFXVol));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Set()
+    {
+        noSet = noSet - 1;
+
+        if (noSet > 0 && noSet < 2)
+        {
+            Strat.SetActive(false);
+        }
     }
 }
